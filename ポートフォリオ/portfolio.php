cb96b1a32dda90b8d90ce6323e1552a49e9d6f13
tetsuya0608 <tetsuya.a.w.80@gmail.com>
2020-01-16 16:26:56 +0900

    portfolio commit

---
+++ ポートフォリオ/portfolio.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>reserveData</title>
+  </head>
+  <body>
+  <h2><?php echo $_POST['name'] ?>さんお問い合わせありがとうございます！</h2>
+
+  </body>
+</html>
